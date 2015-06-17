c1144daefac1e1fa76955458f9648f5040078fff
hb08 <fated.decree@gmail.com>
2015-06-16 20:59:27 -0400

    SQL Pulls for Garden Plotter
    p5.js hooks working, canvas resizes to user input with square grid,
    select pulls from user lists and user input.

--- app/controllers/PlotController.php
+++ app/controllers/PlotController.php
@@ -13,6 +13,49 @@ class PlotController extends BaseController {
 	public function newPlot()
 	{
 		$input = Input::all();
-		return View::make('pages.plot_new', array('title' => 'New Garden | Flourish – Your Florida Gardening Guide', 'input' => $input));
+		// Array waiting for Garden Numbers
+		$gardenNumber = array();
+		$uid = 9; // CHANGE FOR USER INTERACTION
+		foreach($input as $i){
+			// If gardens have been chosen
+			if(strpos($i,'garden_') !== false ){
+				// Remove garden_
+					$g = str_replace('garden_', '', $i);
+				// And push to the GardenNumber array
+					array_push($gardenNumber, $g);
+			}
+		}
+		// Array waiting for plants
+		$plants = array();
+		$z;
+		// Go through each garden and add all the plants to one list
+		foreach($gardenNumber as $n){
+			// If this is a zip code
+			if($n == 'zip'){
+				// Pull all for zip code
+				// $z = DB::select('SELECT zone_number FROM users JOIN zip_zone ON users.zip_code=zip_zone.zipcode WHERE user_id = ?', [$uid] );
+				// TEMP ZONE For CURRENT PLANTS
+				$z = 9;
+				$p = DB::table('plant_list')->where('zone_id', $z)->lists('plant_name');
+				foreach($p as $pl){
+					array_push($plants, $pl);
+				}
+			}else{
+			// All other zones
+				$p = DB::table('plant_list')->leftJoin('user_plants', 'plant_list.plant_id', '=', 'user_plants.plant_id')->lists('plant_name');
+				foreach($p as $pl){
+					array_push($plants, $pl);
+				}
+			}
+		}
+		// Create empty NewList
+		$newList = array();
+		// Go through each item in plants and push as individual value to array
+		foreach($plants as $pl){
+			array_push($newList, $pl);
+		}
+		$newList = array_unique($newList); // Remove any duplicates
+		$newLIst = asort($newList); // Sort alphabetically
+		return View::make('pages.plot_new', array('title' => 'New Garden | Flourish – Your Florida Gardening Guide', 'input' => $input, 'plants' => $newList));
 	}
 }
--- app/views/pages/plot_new.blade.php
+++ app/views/pages/plot_new.blade.php
@@ -3,80 +3,71 @@
 <div class="row">
   <h1 class="pageTitle">Garden Plotter</h1>
 </div>
-<div class="row panel_plot">
-  <div class="row inlay">
-      <div class="row">
-        <h2>New Garden</h2>
+<div class="row">
+    <div class="row">
+      <div class="medium-3 columns"> <!-- Plant/Shape Selection -->
+        <p>Select a plant from the drop down box, then click on a shape.</p>
+        <select class="medium-12 columns">
+          @foreach($plants as $p)
+            <option value={{$p}}>{{$p}}</option>
+          @endforeach
+        </select>
+        <p>Shapes</p>
+        <a href="#" class="linkButton">Save Garden</a>
       </div>
-      <div class="row">
-        <div class="row">
-          <p>Select a plant from the drop down box, then click on a shape.</p>
+      <div class="medium-8 end columns"> <!-- Canvas -->
+        <div id="canvas_frame">
+          <script>
+          function setup() {
+            // Grid Setup
+            var lineColor = 'rgba(9, 59, 13, 0.33)';
+            var gx = getElement('width'); // Width of Garden from user
+            gx = gx.html();
+            var gy = getElement('height'); // Height of Garden from user
+            gy = gy.html();
+
+            // Canvas Size
+            var gw = 700; // Chart Max Width ideal
+            var gh = 500; // Chart Max Height ideal
+
+            var xlength = floor(gw/gx); // Width of Grid Square
+            var ylength = floor(gh/gy); // Height of Grid Square
+
+            var cellSize;
+            if(xlength <= ylength){
+              cellSize = xlength;
+            }else {
+              cellSize = ylength;
+            }
+
+            // Create the canvas
+            var cnv = createCanvas(gx * cellSize, gy * cellSize);
+            var div = document.getElementById('canvas_frame');
+            cnv.parent(div).id('gardenCanvas');
+            background('white');
+
+              // Horizontal Lines
+             for(i = 1; i < gx+1 ; i++ ){ // i is equal to 1, run loop until i = gx
+               stroke(lineColor);
+               line( 0, i * cellSize, gw, i * cellSize );
+             }
+             // Vertical Lines
+             for(i = 1; i < gy+1 ; i++ ){ // i is equal to 1, run loop until i = gx
+               stroke(lineColor);
+               line( i * cellSize, 0, i * cellSize, gh);
+             }
+
+
+          }
+          function draw() {
+
+          }
+          </script>
         </div>
-        <div class="row">
-          <div class="medium-3 columns"> <!-- Plant/Shape Selection -->
-            <select>
-
-              <option></option>
-
-            </select>
-            <p>Shapes</p>
-            <a href="#" class="linkButton">Save Garden</a>
-          </div>
-          <div class="medium-8 end columns"> <!-- Canvas -->
-            <div id="canvas_frame">
-              <script>
-              function setup() {
-                // Grid Setup
-                var lineColor = 'rgba(9, 59, 13, 0.33)';
-                var gx = getElement('width'); // Width of Garden from user
-                gx = gx.html();
-                var gy = getElement('height'); // Height of Garden from user
-                gy = gy.html();
-
-                // Canvas Size
-                var gw = 700; // Chart Width ideal
-                var gh = 500; // Chart Height ideal
-
-                var xlength = floor(gw/gx); // Width of Grid Square
-                var ylength = floor(gh/gy); // Height of Grid Square
-
-                // Create the canvas
-                var cnv = createCanvas(gx * xlength, gy * ylength);
-                var div = document.getElementById('canvas_frame');
-                cnv.parent(div).id('gardenCanvas');
-                background('white');
-
-                console.log("Xlength: " + xlength + "| Ylength: " + ylength);
-                  // Horizontal Lines
-                 for(i = 1; i < gx+1 ; i++ ){ // i is equal to 1, run loop until i = gx
-                   stroke(lineColor);
-                   line( 0, i * ylength, gw, i * ylength );
-                 }
-                 // Vertical Lines
-                 for(i = 1; i <= gy; i++ ){ // i is equal to 1, run loop until i = gx
-                   stroke(lineColor);
-                   line( i * xlength, 0, i * xlength, gh);
-                 }
-
-
-              }
-              function draw() {
-
-              }
-              </script>
-            </div>
-            <div class="medium-3 columns medium-offset-9">
-              <p><span id="width">{{$input["width"]}}</span> x <span id="height">{{$input["height"]}}</span></p>
-            </div>
-          </div>
+        <div class="medium-3 columns medium-offset-9">
+          <p><span id="width">{{$input["width"]}}</span> x <span id="height">{{$input["height"]}}</span></p>
         </div>
       </div>
-        <pre>
-          {{ print_r($input) }}
-        </pre>
-
-      </div>
-  </div> <!-- End Inlay -->
-</div> <!-- End Panel Plot -->
+    </div>
 </div> <!-- End Row -->
 @stop
